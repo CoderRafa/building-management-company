a4f9638fc321814586550b01f3b0c6fca551d049
CoderRafa <rafar2005@yandex.ru>
2022-10-30 09:08:46 +0200

    Init commit

---
+++ src/main/kotlin/com/gmail/rafengimprove/building/managing/company/BuildingManagingCompanyApplication.kt
@@ -0,0 +1,11 @@
+package com.gmail.rafengimprove.building.managing.company
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BuildingManagingCompanyApplication
+
+fun main(args: Array<String>) {
+    runApplication<BuildingManagingCompanyApplication>(*args)
+}
---
+++ src/test/kotlin/com/gmail/rafengimprove/building/managing/company/BuildingManagingCompanyApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.gmail.rafengimprove.building.managing.company
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BuildingManagingCompanyApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
